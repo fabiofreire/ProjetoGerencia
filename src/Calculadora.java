670159e8c671a1a9f64535dc619793801a4c072a
Fábio Freire da Silva <fabricio_elton@hotmail.com>
2013-05-15 10:48:28 -0300

    tirando erro metodo multiplicar classe calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -9,7 +9,7 @@ public class Calculadora {
         }
         
         public int multiplica(int a, int b) {
-                return a / b;
+                return a * b;
         }
         
         public double divide(int a, int b) {
