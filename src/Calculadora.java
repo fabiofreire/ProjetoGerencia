e0a491fb5d2ee1816e3b5f2e359632e03ea6e201
Fábio Freire da Silva <fabricio_elton@hotmail.com>
2013-05-15 10:42:21 -0300

    fazendo alteracoes necessarias

---
+++ src/Calculadora.java
@@ -0,0 +1,18 @@
+public class Calculadora {
+        
+        public int soma(int a , int b) {
+                return a + b;
+        }
+        
+        public int subtrai(int a, int b) {
+                return a - b;
+        }
+        
+        public int multiplica(int a, int b) {
+                return a / b;
+        }
+        
+        public double divide(int a, int b) {
+                return a / b;
+        }
+}
---
+++ src/CalculadoraTest.java
@@ -0,0 +1,50 @@
+import junit.framework.TestCase;
+
+public class CalculadoraTest extends TestCase {
+        
+        private Calculadora calculadora;
+        
+        protected void setUp() throws Exception {
+                super.setUp();
+                calculadora = new Calculadora();
+        }
+        
+        protected void tearDown() throws Exception {
+                super.tearDown();
+                calculadora = null;
+        }
+        
+        public void testSoma() {
+                assertEquals(15, calculadora.soma(10, 5));
+                assertEquals(5, calculadora.soma(10, -5));
+                assertEquals(-5, calculadora.soma(-10, 5));
+                assertEquals(-15, calculadora.soma(-10, -5));
+        }
+        
+        public void testSubtrai() {
+                assertEquals(5, calculadora.subtrai(10, 5));
+                assertEquals(15, calculadora.subtrai(10, -5));
+                assertEquals(-15, calculadora.subtrai(-10, 5));
+                assertEquals(-5, calculadora.subtrai(-10, -5));
+        }
+        
+        public void testMultiplica() {
+                assertEquals(50, calculadora.multiplica(10, 5));
+                assertEquals(-50, calculadora.multiplica(10, -5));
+                assertEquals(-50, calculadora.multiplica(-10, 5));
+                assertEquals(50, calculadora.multiplica(-10, -5));
+        }
+        
+        public void testDivide() {
+                try {
+                        calculadora.divide(10, 0);
+                        fail("Deveria ter lançado exceção pois denominador é 0.");
+                } catch(IllegalArgumentException e) {}
+                
+                assertEquals(2.0, calculadora.divide(10, 5));
+                assertEquals(-2.0, calculadora.divide(10, -5));
+                assertEquals(-2.0, calculadora.divide(-10, 5));
+                assertEquals(2.0, calculadora.divide(-10, -5));
+        }
+        
+}
